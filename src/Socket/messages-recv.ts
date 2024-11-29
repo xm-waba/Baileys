d7d1101f2321b916ce1b563e2e68dcb6e16b6033
devlikepro <waha@devlike.pro>
2024-11-29 14:43:48 +0700

    [WAHA] Ignore ack on not open connection

--- src/Socket/messages-recv.ts
+++ src/Socket/messages-recv.ts
@@ -92,6 +92,11 @@ export const makeMessagesRecvSocket = (config: SocketConfig) => {
 	let sendActiveReceipts = false
 
 	const sendMessageAck = async({ tag, attrs, content }: BinaryNode, errorCode?: number) => {
+		// If ws not connected - logs it and return
+		if(!ws.isOpen) {
+			logger.warn({ attrs: attrs }, 'Client not connected, cannot send ack')
+			return
+		}
 		const stanza: BinaryNode = {
 			tag: 'ack',
 			attrs: {
